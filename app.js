191f039e783c98f43c204b9da9c793d6a2c195a7
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-05 18:03:25 -0400

    listening on dynamic port so ready for deployment

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import { postController } from './controllers/posts/post-controller.js';
 import { userController } from './controllers/users/user-controller.js';
 
 const app = express()
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-postController(app);
-userController(app);
+postController(app)
+userController(app)
 
-console.log('listening on port 4000');
-app.listen(4000);
+console.log('listening on port 4000')
+app.listen(process.env.PORT || 4000)
