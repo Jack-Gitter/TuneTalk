603cae985d1a6d30dd2c3dae6c86db1673103d3b
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-05 19:51:18 -0400

    both database and webserver deployed to mongo and render.com respectively. Testing can occur on either local or remote environment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { postController } from './controllers/posts/post-controller.js';
 import { userController } from './controllers/users/user-controller.js';
 
 const app = express()
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/WebdevFinal'
 
 app.use(cors())
 app.use(express.json())
@@ -15,4 +16,4 @@ userController(app)
 
 console.log('listening on port 4000')
 app.listen(process.env.PORT || 4000)
-mongoose.connect('mongodb://127.0.0.1:27017/WebdevFinal')
+mongoose.connect(CONNECTION_STRING)
