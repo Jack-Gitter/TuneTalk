6ac6aecad9f05e0a391c9e414ba6553159464d36
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-06 20:50:50 -0400

    removed more user id stuff

--- dao/dao.js
+++ dao/dao.js
@@ -4,19 +4,16 @@ import usersModel from "../models/users-model.js";
 export const getAllPosts = async () => await postsModel.find()
 export const getPost = async (postID) => await postsModel.findById(postID)
 export const createPost = async (post) => await postsModel.create(post)
-export const updatePost = async (pid, post) => await postsModel.updateOne({_id: pid}, {$set: post})
+export const updatePost = async (pid, post) => await postsModel.updateOne({_id: pid}, post)
 export const deletePost = async (pid) => await postsModel.deleteOne({_id: pid})
 
 // USERS METHODS START
 
 export const getAllUsers = async() => await usersModel.find()
-export const getUserByID = async(userID) => await usersModel.findById(userID)
 export const getUserByUsername = async(username) => await usersModel.findOne({username: username})
 export const getUserByParams = async(user) => await usersModel.findOne({
     username: user.username, password: user.password, email: user.email 
 })
 export const createUser = async (user) => await usersModel.create(user)
-export const updateUserById = async (uid, user) => await usersModel.updateOne({_id: uid}, {$set: user})
-export const updateUserByUsername = async (username, user) => await usersModel.updateOne({username: username}, {$set: user})
-export const deleteUserById = async (uid) => await usersModel.deleteOne({_id: uid})
+export const updateUserByUsername = async (username, user) => await usersModel.updateOne({username: username}, user)
 export const deleteUserByUsername = async (username) => await usersModel.deleteOne({username: username})
