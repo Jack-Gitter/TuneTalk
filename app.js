61cb1200faab0aac89e645687a25e5461d18c91b
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-19 16:14:31 -0400

    try

--- app.js
+++ app.js
@@ -10,16 +10,17 @@ import { userController } from './controllers/users/user-controller.js';
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/WebdevFinal'
 
 const app = express()
+app.use(session({
+    secret: 'secret',
+    resave: false, 
+    saveUninitialized: true,
+    cookie: {secure: false}
+}))
 app.use(cors(
     {
         credentials: true,
     }
 ))
-app.use(session({
-    secret: 'secret',
-    resave: true, 
-    saveUninitialized: true,
-}))
 
 app.set('trust proxy', 1)
 app.use(express.json())
