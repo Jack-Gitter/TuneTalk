83004c7523d0b1e73eedbcef54d783dfbeaf6768
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-19 14:08:45 -0400

    testing

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(session({
     resave: true, 
     saveUninitialized: true,
 }))
+
+app.use(function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+})
 app.set('trust proxy', 1)
 app.use(express.json())
 
