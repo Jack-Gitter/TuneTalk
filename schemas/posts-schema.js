74c427fbe3ad3b5eeb042c93cc849b5e671147bc
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-20 21:13:54 -0400

    shuffle maybe work?

--- schemas/posts-schema.js
+++ schemas/posts-schema.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const postsSchema = mongoose.Schema({
     songTitle: {type: String, required: true},
     review: {type: String, required: true},
-    username: {type: String, required: true},
     rating: {type: Number, min: 0, max: 5, required: true},
+    username: {type: String, required: true},
     artists: Array,
     time: String,
     genre: String,
